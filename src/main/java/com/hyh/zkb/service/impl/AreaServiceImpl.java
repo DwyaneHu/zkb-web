6107ad295af7c5dbc5a9ae87fa91690e84577ed8
DwyaneHu <185434926@qq.com>
2016-07-19 16:53:19 +0800

    更改数据库配置,更改pom配置

--- src/main/java/com/hyh/zkb/service/impl/AreaServiceImpl.java
+++ src/main/java/com/hyh/zkb/service/impl/AreaServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.baidu.unbiz.common.ObjectUtil;
-import com.baidu.unbiz.common.StringUtil;
 import com.hyh.zkb.dao.AreaMapper;
 import com.hyh.zkb.model.Area;
 import com.hyh.zkb.service.IAreaService;
@@ -16,19 +14,15 @@ public class AreaServiceImpl implements IAreaService {
 
 	@Autowired
 	private AreaMapper areaMapper;
-	
-	
-	
+
 	public int addArea(Area area) {
 		// TODO Auto-generated method stub
-		if(ObjectUtil.isNotNull(area) && StringUtil.isNotEmpty(area.getName()) && ObjectUtil.isNotNull(area.getLevel()))
-			return areaMapper.insertSelective(area);
-		return -1;
+		return areaMapper.insertSelective(area);
 	}
 
 	public void deleteArea(int id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Area selectAreaById(int id) {
@@ -50,5 +44,5 @@ public class AreaServiceImpl implements IAreaService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
